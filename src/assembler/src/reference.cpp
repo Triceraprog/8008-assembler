66294a7f85f1d68e8d9f5f59cb1d92a8cb87fed4
Sylvain Glaize <mokona@puupuu.org>
2022-01-19 10:50:05 +0100

    Extract file opening.

--- src/assembler/src/reference.cpp
+++ src/assembler/src/reference.cpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <iostream>
 
 /* this is a lot of symbols, but needed for SCELBAL */
 #define MAXSYMBOLS 1000
@@ -551,10 +552,80 @@ int finddata(char* line, int* outdata)
     return (n);
 }
 
+class Files
+{
+public:
+    explicit Files(const Options& options)
+    {
+        set_output_filenames(options);
+        open_files(options);
+    }
+
+    ~Files() {}
+
+private:
+    void set_output_filenames(const Options& options)
+    {
+        const auto* output_filename_extension =
+                global_options.generate_binary_file ? ".bin" : ".hex";
+        outfilename = global_options.input_filename_base + output_filename_extension;
+        listfilename = global_options.input_filename_base + ".lst";
+        input_filename = global_options.input_filename;
+
+        if (global_options.debug)
+        {
+            std::cout << "filebase=" << global_options.input_filename_base << " ";
+            std::cout << "infile=" << input_filename << " ";
+            std::cout << "outfile=" << outfilename << " ";
+            std::cout << "listfile=" << listfilename << "\n";
+        }
+    }
+
+    void open_files(const Options& options)
+    {
+        if ((ifp = fopen(input_filename.c_str(), "rt")) == nullptr)
+        {
+            fprintf(stderr, "Can't open %s as input file\n", global_options.input_filename.c_str());
+            exit(-1);
+        }
+        if (options.generate_binary_file)
+        {
+            if ((ofp = fopen(outfilename.c_str(), "wb")) == nullptr)
+            {
+                fprintf(stderr, "Can't open %s as output file\n", outfilename.c_str());
+                exit(-1);
+            }
+        }
+        else
+        {
+            if ((ofp = fopen(outfilename.c_str(), "wt")) == nullptr)
+            {
+                fprintf(stderr, "Can't open %s as output file\n", outfilename.c_str());
+                exit(-1);
+            }
+        }
+        if (options.generate_list_file)
+        {
+            if ((lfp = fopen(listfilename.c_str(), "wt")) == nullptr)
+            {
+                fprintf(stderr, "Can't open %s as input file\n", listfilename.c_str());
+                exit(-1);
+            }
+        }
+        if (options.debug)
+        {
+            printf("All files were opened.\n");
+        }
+    }
+
+    std::string outfilename;
+    std::string listfilename;
+    std::string input_filename;
+};
+
 int main(int argc, const char** argv)
 {
 
-    char outfilename[80], listfilename[80];
     char line[100], cleanline[100], label[20];
     char opcode[80], arg1str[20], arg2str[20], c, *cptr;
     char singlespacepad[9]; /* this is some extra padding if we use single space list file */
@@ -583,54 +654,7 @@ int main(int argc, const char** argv)
         strcpy(singlespacepad, "        ");
 
     /* write either hex file or binary file */
-    if (global_options.generate_binary_file)
-        sprintf(outfilename, "%s.bin", global_options.input_filename_base.c_str());
-    else
-        sprintf(outfilename, "%s.hex", global_options.input_filename_base.c_str());
-    sprintf(listfilename, "%s.lst", global_options.input_filename_base.c_str());
-    if (global_options.debug)
-        printf("global_options.input_filename_base.c_str()=\"%s\" infile=\"%s\" outfile=\"%s\" "
-               "listfile=\"%s\"\n",
-               global_options.input_filename_base.c_str(), global_options.input_filename.c_str(),
-               outfilename, listfilename);
-    /*
-   *
-   * Okay, now we have options processed, and we know file names.
-   * Now let's open all files.
-   *
-   */
-
-    if ((ifp = fopen(global_options.input_filename.c_str(), "rt")) == nullptr)
-    {
-        fprintf(stderr, "Can't open %s as input file\n", global_options.input_filename.c_str());
-        exit(-1);
-    }
-    if (global_options.generate_binary_file)
-    {
-        if ((ofp = fopen(outfilename, "wb")) == nullptr)
-        {
-            fprintf(stderr, "Can't open %s as output file\n", outfilename);
-            exit(-1);
-        }
-    }
-    else
-    {
-        if ((ofp = fopen(outfilename, "wt")) == nullptr)
-        {
-            fprintf(stderr, "Can't open %s as output file\n", outfilename);
-            exit(-1);
-        }
-    }
-    if (global_options.generate_list_file)
-    {
-        if ((lfp = fopen(listfilename, "wt")) == nullptr)
-        {
-            fprintf(stderr, "Can't open %s as input file\n", listfilename);
-            exit(-1);
-        }
-    }
-    if (global_options.debug)
-        printf("opened all files\n");
+    Files files(global_options);
 
     /*
    *
